efceacb44ae7cf6ed959adc291824e9bdb2aaf90
Ziegel999 <christopher@capito.rocks>
2019-12-09 02:03:34 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Container, Row, Card, Col } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container fluid={true} style={{ height: '100vh' }}>
+        <Row>    
+          <Card className='mx-auto'>
+            <Row>           
+          <Col>       
+              <Row>
+                <Button variant='primary'>Sound 1</Button>
+                <Button variant='primary'>Sound 2</Button>
+                <Button variant='primary'>Sound 3</Button>
+              </Row>
+              <Row>
+                <Button variant='primary'>Sound 4</Button>
+                <Button variant='primary'>Sound 5</Button>
+                <Button variant='primary'>Sound 6</Button>
+              </Row>
+              <Row>
+                <Button variant='primary'>Sound 7</Button>
+                <Button variant='primary'>Sound 8</Button>
+                <Button variant='primary'>Sound 9</Button>
+              </Row> 
+              </Col>
+              <Col>
+              Hello</Col>  
+              </Row>       
+          </Card>  
+          </Row>      
+      </Container>
     </div>
   );
 }
